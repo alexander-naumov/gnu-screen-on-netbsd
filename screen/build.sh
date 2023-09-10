42f13d9768c6849a6d2d6e3c681626e2743a97fe
Alexander Naumov <alexander_naumov@opensuse.org>
2023-09-10 20:45:03 +0200

    fix PATH to sources dir

--- screen/build.sh
+++ screen/build.sh
@@ -5,7 +5,7 @@ set -exu
 uname -a
 cc -v
 git -c http.sslVerify=false clone https://github.com/alexander-naumov/gnu-screen.git
-cd screen/src
+cd gnu-screen/src
 ./autogen.sh
 ./configure CFLAGS="-Wall -DDEBUG"
 gmake
