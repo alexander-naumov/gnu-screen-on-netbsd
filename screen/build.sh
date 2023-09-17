7f2b2ca3bfabde471aa1d874c92a560ac5272f9a
Alexander Naumov <alexander_naumov@opensuse.org>
2023-09-17 21:45:31 +0200

    come back to clang-only flow

--- screen/build.sh
+++ screen/build.sh
@@ -7,15 +7,16 @@ git -c http.sslVerify=false clone https://github.com/alexander-naumov/gnu-screen
 cd gnu-screen/src
 ./autogen.sh
 
-gcc -v
-cc=gcc ./configure CFLAGS="-Wall"
-gmake
-./screen -v
-./screen -ls || echo $?
-gmake clean
+#gcc -v
+#cc=gcc ./configure CFLAGS="-Wall"
+#gmake
+#./screen -v
+#./screen -ls || echo $?
+#gmake clean
 
 clang -v
 cc=clang ./configure CFLAGS="-Wall -DDEBUG"
 gmake
 ./screen -v
+./screen -ls || echo $?
 ./screen --help
